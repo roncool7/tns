2ae985f1b399f1663cc1913b9936d996ea37c997
roncool7 <roncool77777@gmail.com>
2020-10-27 01:46:15 +0200

    env fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true
 };
+
+export const contactBaseUrl = "http://localhost:3000/api/contact";
+export const authBaseUrl = "http://localhost:3000/api/auth";
+export const auctionBaseUrl = "http://localhost:3000/api/auction";
+export const productsBaseUrl = "http://localhost:3000/api/products";
+export const productsAgesBaseUrl = "http://localhost:3000/api/productsAges";
+export const productsBrandsBaseUrl = "http://localhost:3000/api/productsBrands";
+export const searchBaseUrl = "http://localhost:3000/api/search";
